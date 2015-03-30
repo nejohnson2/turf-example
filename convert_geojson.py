7aeb06f41baa39dff354ae89df5e674cb842ce93
Gioia <Gioia@172-16-24-84.DYNAPOOL.NYU.EDU>
2015-03-30 09:16:33 -0400

    updated script to include properties

--- convert_geojson.py
+++ convert_geojson.py
@@ -11,7 +11,8 @@ data = pd.read_csv('11222_subset.csv')
 
 # -- Function for creating tuple
 def create_geojson(data):
-	x = (data['Longitude'], data['Latitude'])
+	coords = (data['Longitude'], data['Latitude'])
+	
 	prop = {
 		"created_date": data['Created Date'], 
 		"complaint_type": data['Complaint Type'],
@@ -19,18 +20,19 @@ def create_geojson(data):
 		"agency_name": data['Agency Name'],
 		"descriptor": data['Descriptor']
 	}
-	return [x,prop]
+	
+	return [coords,prop]
 	
 
 # -- Turn function into array
-z = [Feature(geometry=Point(i[0], properties=i[1])) for i in data.apply(create_geojson, axis=1)]
+geojson = [Feature(geometry=Point(i[0], properties=i[1])) for i in data.apply(create_geojson, axis=1)]
 
 
 # -- File to output
 out_file = open("311.geojson","w")
 
 
-json.dump(FeatureCollection(z), out_file)                                    
+json.dump(FeatureCollection(geojson), out_file)                                    
 
 
 # Close the file
